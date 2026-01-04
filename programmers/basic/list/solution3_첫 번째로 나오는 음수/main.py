4cca0bca04908b8f5f2cbb5f08870016836958fa
Jayyxy <mimichoi0121@gmail.com>
2026-01-05 06:53:45 +0900

    [basic] 첫번째로 나오는 음수

---
+++ programmers/basic/list/solution3_첫 번째로 나오는 음수/main.py
@@ -0,0 +1,18 @@
+def solution(num_list):
+    i = 0
+    answer = -1  
+    
+    # 리스트의 인덱스값, 리스트값 둘다 필요 
+    for i in range(len(num_list)) : 
+        
+        if num_list[i] <0 : 
+             
+            #return혹은 break로 끝내야함 
+            return i
+        
+        
+        
+        
+
+    
+    return answer
